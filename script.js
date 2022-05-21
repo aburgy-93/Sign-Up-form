ccabc0e6504d2d488461799ab086abe90bfbd6b7
Alex Burgy <burgyburger@gmail.com>
2022-05-20 19:05:41 -0500

    VScode is making me

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const createAccountButton = document.querySelector(".create-account");
+
+const firstName = document.querySelector(".input-firstName");
+
+createAccountButton.addEventListener("click", function () {
+  let enterPassword = document.querySelector(".enterPassword").value;
+  let confirmPassword = document.querySelector(".confirmPassword").value;
+
+  if (enterPassword === "") {
+    alert("Field Empty.");
+  } else if (enterPassword != confirmPassword) {
+    document.querySelector(".enterPassword").style.borderColor = "red";
+    document.querySelector(".confirmPassword").style.borderColor = "red";
+    document.querySelector(".no-match").style.color = "red";
+  } else if (enterPassword === confirmPassword) {
+    document.querySelector(".enterPassword").style.borderColor = "green";
+    document.querySelector(".confirmPassword").style.borderColor = "green";
+    alert("Logged In.");
+  }
+
+  console.log(enterPassword, confirmPassword);
+});
